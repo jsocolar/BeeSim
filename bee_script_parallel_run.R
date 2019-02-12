6678c7362b6fd6c2e0e64dd97f8a18ced5635f1e
Jacob Socolar <jacob.socolar@gmail.com>
2019-02-12 14:21:04 -0600

    fixed bug in parallel run

--- bee_script_parallel_run.R
+++ bee_script_parallel_run.R
@@ -1,6 +1,14 @@
-# Function to call and run bee_script_updated in parallel on 4 cores.
-library(doParallel)
+# Run bee_script_updated() in parallel.
+# THE ENTIRETY OF bee_script_updated.R MUST BE RUN PRIOR TO EXECUTING THIS CODE
 
+library(doParallel)
+# doParallel is the workhorse pacakge that we'll use for parallel computing
+
+# doParallel requires that we express everything we want to do on each parallel core *as a function*.
+# Below, we define the function run_beesim(). Running run_beesim() once will give us one set of simulations for
+# each combination of parameter values that we pass as inputs to run_beesim(). Running run_beesim() multiple times
+# will give us replicate simulations. Using doParallel to run run_beesim() on n cores will return a list of length
+# n with one full replicate simulation (including all parameter values) as each element of the list.
 run_beesim <- function(bee_logmean1, bee_logmean2, bee_logsd1, bee_logsd2, rare_advantage, EP, e21, e12, beefluct, bee_plant_ratio){
   n1 <- length(bee_logmean1)
   n2 <- length(bee_logmean2)
@@ -38,7 +46,7 @@ run_beesim <- function(bee_logmean1, bee_logmean2, bee_logsd1, bee_logsd2, rare_
                     for(i10 in 1:n10){
                       output[[i1]][[i2]][[i3]][[i4]][[i5]][[i6]][[i7]][[i8]][[i9]][[i10]] <-
                         beesim(bee_logmeans=blms, bee_logsds=blss, bee_mean_abunds=c(100,100), 
-                               plant_inits=c(500000,500000), n_years=100, visits_per_bee=c(25000,25000), 
+                               plant_inits=rep(EP/2, 2), n_years=100, visits_per_bee=c(25000,25000), 
                                pollen_transfer_probs=c(1,1,1,1,0.7,0.5,0.2), pollen2plant_prob=c(.2,.2), fixed=FALSE,
                                visitprobtype=1, rare_advantage=rare_advantage[i5], plotting=FALSE, EP=EP[i6], e21=e21[i7], 
                                e12=e12[i8], maxprob=.2, beefluct=beefluct[i9], r_times_k=.3)
@@ -55,18 +63,32 @@ run_beesim <- function(bee_logmean1, bee_logmean2, bee_logsd1, bee_logsd2, rare_
   return(output)
 }
 
-run_beesim_inputs <- list(bee_logmean1 = c(0, 4, 10), bee_logmean2 = c(-10, -4, 0, 4, 10), bee_logsd1 = c(0, 1, 3), bee_logsd2 = c(0, 1, 3), rare_advantage = c(.1, .7, 1, 1.3), EP = c(500000), e21 = c(0, .75, 1), e12 = c(0, .75, 1, 1.5), beefluct = c('none', 'independent', 'plant-based'), bee_plant_ratio = c(.001))
-run_beesim_inputs2 <- list(bee_logmean1 = c(0, 4, 10), bee_logmean2 = c(-10, -4, 0, 4, 10), bee_logsd1 = c(0, 1, 3), bee_logsd2 = c(0, 1, 3), rare_advantage = c(.1, .7, 1, 1.3), EP = c(500000), e21 = c(0, .75, 1), e12 = c(0, .75, 1, 1.5), beefluct = c('none', 'independent', 'plant-based'), bee_plant_ratio = c(.001))
-attach(run_beesim_inputs)
+# We define the inputs that we want to pass to run_beesim(). In this code block, we can specify multiple combinations
+# of inputs to explore different regions of parameter space.
+run_beesim_inputs.1 <- list(bee_logmean1 = c(0, 4, 10), bee_logmean2 = c(-10, -4, 0, 4, 10), bee_logsd1 = c(0, 1, 3), bee_logsd2 = c(0, 1, 3), rare_advantage = c(.1, .7, 1, 1.3), EP = c(500000), e21 = c(0, .75, 1), e12 = c(0, .75, 1, 1.5), beefluct = c('none', 'independent', 'plant-based'), bee_plant_ratio = c(.001))
+run_beesim_inputs.2 <- list(bee_logmean1 = c(0, 4, 10), bee_logmean2 = c(-10, -4, 0, 4, 10), bee_logsd1 = c(0, 1, 3), bee_logsd2 = c(0, 1, 3), rare_advantage = c(.1, .7, 1, 1.3), EP = c(500000), e21 = c(0, .75, 1), e12 = c(0, .75, 1, 1.5), beefluct = c('none', 'independent', 'plant-based'), bee_plant_ratio = c(.001))
 
-s <- proc.time()
-cl <- makeCluster(4)
-registerDoParallel(cl)
-bee_output <- foreach(i=1:4) %dopar% run_beesim(bee_logmean1 = bee_logmean1, bee_logmean2 = bee_logmean2,
-                                                bee_logsd1 = bee_logsd1, bee_logsd2 = bee_logsd2,
-                                                rare_advantage = rare_advantage, EP = EP, e21 = e21, e12 = e12,
-                                                beefluct = beefluct, bee_plant_ratio = bee_plant_ratio)
-stopCluster(cl)
-elapsed <- proc.time() - s
+# Below, we select which version of run_beesim_inputs.X we want to use in our simulation. 
+run_beesim_inputs <- run_beesim_inputs.1
 
+# Below, we create the cluster and run the job.
+s <- proc.time()                          # for keeping track of how long the job takes
+cl <- parallel::makeCluster(4)            # make a cluster (called cl) with 4 cores. Note that this function comes from the package 'parallel' on which 'doParallel' depends
+doParallel::registerDoParallel(cl)        # some doParallel voodoo to get the cores ready for work
+# Below, we execute run_beesim on our cluster
+bee_output <- foreach(i=1:4) %dopar% run_beesim(bee_logmean1 = run_beesim_inputs$bee_logmean1, 
+                                                bee_logmean2 = run_beesim_inputs$bee_logmean2,
+                                                bee_logsd1 = run_beesim_inputs$bee_logsd1, 
+                                                bee_logsd2 = run_beesim_inputs$bee_logsd2,
+                                                rare_advantage = run_beesim_inputs$rare_advantage, 
+                                                EP = run_beesim_inputs$EP, 
+                                                e21 = run_beesim_inputs$e21, 
+                                                e12 = run_beesim_inputs$e12,
+                                                beefluct = run_beesim_inputs$beefluct, 
+                                                bee_plant_ratio = run_beesim_inputs$bee_plant_ratio)
+parallel::stopCluster(cl)                # Closing down our cluster
+elapsed <- proc.time() - s               # how long the job took
+
+# save output.  IMPORTANT: DO NOT FORGEET TO CHANGE FILENAME WHEN USING DIFFERENT PARAMETER INPUTS.  OTHERWISE
+#                          YOU'LL OVERWRITE THE OLD SIMULATIONS!
 save(bee_output, file = "/Users/JacobSocolar/Dropbox/Work/Beesim_fullrun/bee_output.Rdata")
--- code for making figures_Jan9.R
+++ code for making figures_Jan9.R
@@ -1,205 +1,121 @@
 library(fields)
 library(spam)
-
 library(viridis)
+setwd("/Users/JacobSocolar/Dropbox/Work/Beesim_fullrun/")
+
+load("bee_output.Rdata")
 col <- colorRampPalette(c("navy", "coral"))(120)
 
+attach(bee_output$inputs)
 
-load("C:/Users/betha/Dropbox/Work/Research/Konzmann & Lunau follow up/L&A 1970 follow up - R figures/code/Beesim_fullrun/OCarray7.Rdata")
-bee_logsd1 <- c(0,1,3)
-bee_logsd2 <- c(0,1,3)
+# matrix of panels for split-screen with a 3x3 panel matrix to plot each combo of bee preference sds, and extra panels
+# of x and y axis labels
+splits <- rbind(c(0,1,0,.1), # x axis label
+                c(0,.1,0,1), # y axis label
+                
+                c(.1,.4,.1,.4), # bottom left
+                c(.1,.4,.4,.7), # middle left
+                c(.1,.4,.7,1), # top left
 
-rare_advantage <- seq(.1,1.9,.2)
-rare_advantage <- round(10*rare_advantage)/10
-X <- c(0,2,4,6,10)
-pos_x <- c(1:5)
-Y <- c(-10,-6,-4,-2,0,2,4,6,10)
-pos_y <- c(1:9)
+                c(.4,.7,.1,.4), # bottom center
+                c(.4,.7,.4,.7), # middle center
+                c(.4,.7,.7,1), # top center
+                
+                c(.7,1,.1,.4), # bottom right
+                c(.7,1,.4,.7), # middle right
+                c(.7,1,.7,1)) # top right
 
-rA <- c(.1,.5,.9,1.5)
-
-
-pdf(file=paste('C:/Users/betha/Dropbox/Work/Research/Konzmann & Lunau follow up/L&A 1970 follow up - R figures/code/Beesim_fullrun/OCarray7_test_Oct30.Rdata',i1,'.pdf',sep=""), width=50, height=50)
-par(mfrow=c(3,3))
-dev.off()
-split.screen(splits)
-
-ps1 <- 8
-scrnum <- ps1
-
-screen(1)
-text("mean preference; bee sp. 1",x=.5,y=.7)
-lines(x=c(0,1),y=c(.5,.5))
-text("1", x=.5,y=.35,cex=1.3)
-lines(x=c(.5,.5), y=c(.45,.5))
-text("0", x=.14,y=.35,cex=1.3)
-lines(x=c(.14,.14), y=c(.45,.5))
-text("3", x=.86,y=.35,cex=1.3)
-lines(x=c(.86,.86), y=c(.45,.5))
-text("sd preference; bee sp. 1", x=.5, y=.1, cex=1.2)
-
-screen(2)
-text("mean preference; bee sp. 1",x=.5,y=.7)
-lines(x=c(0,1),y=c(.5,.5))
-text("1", x=.5,y=.35,cex=1.3)
-lines(x=c(.5,.5), y=c(.45,.5))
-text("0", x=.14,y=.35,cex=1.3)
-lines(x=c(.14,.14), y=c(.45,.5))
-text("3", x=.86,y=.35,cex=1.3)
-lines(x=c(.86,.86), y=c(.45,.5))
-text("sd preference; bee sp. 1", x=.5, y=.1, cex=1.2)
-
-screen(3)
-text("mean preference; bee sp. 1",x=.5,y=.7)
-lines(x=c(0,1),y=c(.5,.5))
-text("1", x=.5,y=.35,cex=1.3)
-lines(x=c(.5,.5), y=c(.45,.5))
-text("0", x=.14,y=.35,cex=1.3)
-lines(x=c(.14,.14), y=c(.45,.5))
-text("3", x=.86,y=.35,cex=1.3)
-lines(x=c(.86,.86), y=c(.45,.5))
-text("sd preference; bee sp. 1", x=.5, y=.1, cex=1.2)
-
-screen(4)
-text("mean preference; bee sp. 1",x=.5,y=.7)
-lines(x=c(0,1),y=c(.5,.5))
-text("1", x=.5,y=.35,cex=1.3)
-lines(x=c(.5,.5), y=c(.45,.5))
-text("0", x=.14,y=.35,cex=1.3)
-lines(x=c(.14,.14), y=c(.45,.5))
-text("3", x=.86,y=.35,cex=1.3)
-lines(x=c(.86,.86), y=c(.45,.5))
-text("sd preference; bee sp. 1", x=.5, y=.1, cex=1.2)
-
-screen(5)
-text("mean preference; bee sp. 2",x=.7,y=.5, srt=90)
-lines(x=c(.5,.5),y=c(0,1))
-text("1", x=.35,y=.5,cex=1.3, srt=90)
-lines(x=c(.45,.5), y=c(.5,.5))
-text("0", x=.35,y=.13,cex=1.3, srt=90)
-lines(x=c(.45,.5), y=c(.14,.14))
-text("3", x=.35,y=.86,cex=1.3, srt=90)
-lines(x=c(.45,.5), y=c(.86,.86))
-text("sd preference; bee sp. 1", x=.1, y=.5, cex=1.2, srt=90)
-
-
-
-
-screen(6)
-text("mean preference; bee sp. 2",x=.7,y=.5, srt=90)
-lines(x=c(.5,.5),y=c(0,1))
-text("1", x=.35,y=.5,cex=1.3, srt=90)
-lines(x=c(.45,.5), y=c(.5,.5))
-text("0", x=.35,y=.13,cex=1.3, srt=90)
-lines(x=c(.45,.5), y=c(.14,.14))
-text("3", x=.35,y=.86,cex=1.3, srt=90)
-lines(x=c(.45,.5), y=c(.86,.86))
-text("sd preference; bee sp. 1", x=.1, y=.5, cex=1.2, srt=90)
-
-screen(7)
-text("mean preference; bee sp. 2",x=.7,y=.5, srt=90)
-lines(x=c(.5,.5),y=c(0,1))
-text("1", x=.35,y=.5,cex=1.3, srt=90)
-lines(x=c(.45,.5), y=c(.5,.5))
-text("0", x=.35,y=.13,cex=1.3, srt=90)
-lines(x=c(.45,.5), y=c(.14,.14))
-text("3", x=.35,y=.86,cex=1.3, srt=90)
-lines(x=c(.45,.5), y=c(.86,.86))
-text("sd preference; bee sp. 1", x=.1, y=.5, cex=1.2, srt=90)
-
-screen(8)
-text("mean preference; bee sp. 2",x=.7,y=.5, srt=90)
-lines(x=c(.5,.5),y=c(0,1))
-text("1", x=.35,y=.5,cex=1.3, srt=90)
-lines(x=c(.45,.5), y=c(.5,.5))
-text("0", x=.35,y=.13,cex=1.3, srt=90)
-lines(x=c(.45,.5), y=c(.14,.14))
-text("3", x=.35,y=.86,cex=1.3, srt=90)
-lines(x=c(.45,.5), y=c(.86,.86))
-text("sd preference; bee sp. 1", x=.1, y=.5, cex=1.2, srt=90)
-
-
-for(i1 in 1:length(rA)){
-  for(i3 in 1:length(bee_logsd2)){
-    for(i2 in 1:length(bee_logsd1)){
-      scrnum <- scrnum+1
-      screen(scrnum)
-      mymatrix <- OCarray7[i2,,,i3,which(rare_advantage==rA[i1])]
-      par(mar=c(1,1,1,1))
-      image(x=Y, y=X, z=t(mymatrix[pos_x,pos_y]), zlim = c(0,10), col = col, xaxt='n', yaxt='n')
-      if(scrnum %in% (c(1,2,3,10,11,12,19,20,21,28,29,30)+ps1)){axis(side=1,at=c(-10,0,10))}
-      if(scrnum %in% (c(1,4,7,10,13,16,19,22,25,28,31,34)+ps1)){axis(side=2, at=c(0,6,12))}
+coexistence_values <- array(dim=lapply(bee_output$inputs, FUN=length))
+cvs <- rep(NA,4)
+for(i1 in 1:length(bee_output$inputs[[1]])){
+  for(i2 in 1:length(bee_output$inputs[[2]])){
+    for(i3 in 1:length(bee_output$inputs[[3]])){
+      for(i4 in 1:length(bee_output$inputs[[4]])){
+        for(i5 in 1:length(bee_output$inputs[[5]])){
+          for(i6 in 1:length(bee_output$inputs[[6]])){
+            for(i7 in 1:length(bee_output$inputs[[7]])){
+              for(i8 in 1:length(bee_output$inputs[[8]])){
+                for(i9 in 1:length(bee_output$inputs[[9]])){
+                  for(i10 in 1:length(bee_output$inputs[[10]])){
+                    for(j in 1:4){
+                      cvs[j] <- (bee_output$output[[j]][[i1]][[i2]][[i3]][[i4]][[i5]][[i6]][[i7]][[i8]][[i9]][[i10]]$plant_timeseries[101,2] *
+                        bee_output$output[[j]][[i1]][[i2]][[i3]][[i4]][[i5]][[i6]][[i7]][[i8]][[i9]][[i10]]$plant_timeseries[101,3]) > 0
+                    }
+                    coexistence_values[i1,i2,i3,i4,i5,i6,i7,i8,i9,i10] <- mean(cvs)
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
     }
   }
 }
 
-
+# Make sure we aren't dealing with the vestiges of a previous plotting object or screen splitting
 dev.off()
 
-#LRBT
-splits <- rbind(c(.1,.4,.6,.7),
-                c(.6,.9,.6,.7),
-                c(.1,.4,.1,.2),
-                c(.6,.9,.1,.2),
-                
-                c(0,.1,.7,1),
-                c(0,.1,.2,.5),
-                c(.5,.6,.7,1),
-                c(.5,.6,.2,.5),
-                
-                
-                
-                c(.1,.2,.7,.8),
-                c(.2,.3,.7,.8),
-                c(.3,.4,.7,.8),
-                c(.1,.2,.8,.9),
-                c(.2,.3,.8,.9),
-                c(.3,.4,.8,.9),
-                c(.1,.2,.9,1),
-                c(.2,.3,.9,1),
-                c(.3,.4,.9,1),
-                
-                c(.6,.7,.7,.8),
-                c(.7,.8,.7,.8),
-                c(.8,.9,.7,.8),
-                c(.6,.7,.8,.9),
-                c(.7,.8,.8,.9),
-                c(.8,.9,.8,.9),
-                c(.6,.7,.9,1),
-                c(.7,.8,.9,1),
-                c(.8,.9,.9,1),
-                                
-                c(.1,.2,.2,.3),
-                c(.2,.3,.2,.3),
-                c(.3,.4,.2,.3),
-                c(.1,.2,.3,.4),
-                c(.2,.3,.3,.4),
-                c(.3,.4,.3,.4),
-                c(.1,.2,.4,.5),
-                c(.2,.3,.4,.5),
-                c(.3,.4,.4,.5),
-                
-                c(.6,.7,.2,.3),
-                c(.7,.8,.2,.3),
-                c(.8,.9,.2,.3),
-                c(.6,.7,.3,.4),
-                c(.7,.8,.3,.4),
-                c(.8,.9,.3,.4),
-                c(.6,.7,.4,.5),
-                c(.7,.8,.4,.5),
-                c(.8,.9,.4,.5)
-                
-
-              
-                )
-
-
-
-
-
-
-
-
-
-
-
+for(i5 in 1:length(bee_output$inputs$rare_advantage)){
+  for(i6 in 1:length(bee_output$inputs$EP)){
+    for(i7 in 1:length(bee_output$inputs$e21)){
+      for(i8 in 1:length(bee_output$inputs$e12)){
+        for(i9 in 1:length(bee_output$inputs$beefluct)){
+          for(i10 in 1:length(bee_output$inputs$bee_plant_ratio)){
+            close.screen(all.screens=T)
+            pdf(file=paste("bee_output_plotting",i5,i6,i7,i8,i9,i10, ".pdf", sep="_"), width=13, height=15)
+            
+            split.screen(splits)
+            
+            screen(1)
+            text("mean preference; bee sp. 1",x=.5,y=.7)
+            lines(x=c(0,1),y=c(.5,.5))
+            text("1", x=.5,y=.35,cex=1.3)
+            lines(x=c(.5,.5), y=c(.45,.5))
+            text("0", x=.14,y=.35,cex=1.3)
+            lines(x=c(.14,.14), y=c(.45,.5))
+            text("3", x=.86,y=.35,cex=1.3)
+            lines(x=c(.86,.86), y=c(.45,.5))
+            text("sd preference; bee sp. 1", x=.5, y=.1, cex=1.2)
+            
+            screen(2)
+            text("mean preference; bee sp. 2",x=.7,y=.5, srt=90)
+            lines(x=c(.5,.5),y=c(0,1))
+            text("1", x=.35,y=.5,cex=1.3, srt=90)
+            lines(x=c(.45,.5), y=c(.5,.5))
+            text("0", x=.35,y=.13,cex=1.3, srt=90)
+            lines(x=c(.45,.5), y=c(.14,.14))
+            text("3", x=.35,y=.86,cex=1.3, srt=90)
+            lines(x=c(.45,.5), y=c(.86,.86))
+            text("sd preference; bee sp. 2", x=.1, y=.5, cex=1.2, srt=90)
+            
+            
+            scrnum <- 2
+            for(sd1 in 1:3){
+              for(sd2 in 1:3){
+                scrnum <- scrnum+1
+                screen(scrnum)
+                par(mar=rep(1.5, 4))
+                mymatrix <- t(coexistence_values[,,sd1,sd2,i5,i6,i7,i8,i9,i10])
+                mymatrix <- mymatrix[nrow(mymatrix):1, ]
+                xvals <- c(rep(bee_output$inputs$bee_logmean1[1], length(bee_output$inputs$bee_logmean2)),
+                           rep(bee_output$inputs$bee_logmean1[2], length(bee_output$inputs$bee_logmean2)),
+                           rep(bee_output$inputs$bee_logmean1[3], length(bee_output$inputs$bee_logmean2)))
+                yvals <- rep(rev(bee_output$inputs$bee_logmean2), length(bee_output$inputs$bee_logmean1))
+                mv <- as.vector(mymatrix)
+                for(k in 1:length(mv)){
+                  mv[k] <- plasma(100)[min(which(c(1:100) >= (25 + 50*as.numeric(mv[k]))))]
+                }
+                plot(xvals, yvals, pch=19, col=mv, cex=3, axes = F, xlab='', ylab='', xlim=c(min(bee_output$inputs$bee_logmean1) - 1,max(bee_output$inputs$bee_logmean1) + 2),
+                     ylim=c(min(bee_output$inputs$bee_logmean2) - 1,max(bee_output$inputs$bee_logmean2) + 2))
+                axis(side=1, at=bee_output$inputs$bee_logmean1)
+                axis(side=2, at=bee_output$inputs$bee_logmean2)
+              }
+            }
+            dev.off()
+          }
+        }
+      }
+    }
+  }
+}
